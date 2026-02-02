73a075b812fdb29a1b1499edb34c3096bd94d563
ehabimfura <e.habimfura@alustudent.com>
2026-02-02 07:52:58 -0800

    write on a file 3

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -5,12 +5,7 @@ const fs = require('fs');
 const filePath = process.argv[2];
 const content = process.argv.slice(3).join(' ');
 
-if (!filePath || !content) {
-  console.error('Please provide both a file path and a string to write.');
-  process.exit(1);
-}
-
-// Write the string to the file in UTF-8
+// Attempt to write the file in UTF-8
 fs.writeFile(filePath, content, 'utf-8', (err) => {
   if (err) {
     console.error(err);
