264368509cdab87f4c0f28e2226b4a37eedb71b5
ehabimfura <e.habimfura@alustudent.com>
2026-02-02 07:27:09 -0800

    semi standard

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,25 +1,25 @@
 #!/usr/bin/node
 
 // Import the 'fs' module to work with the file system
-const fs = require('fs')
+const fs = require('fs');
 
 // Get the file path from the command line arguments
-const filePath = process.argv[2]
+const filePath = process.argv[2];
 
 if (!filePath) {
-  console.error('Please provide a file path as the first argument.')
-  process.exit(1)
+  console.error('Please provide a file path as the first argument.');
+  process.exit(1);
 }
 
 // Read the file in UTF-8 encoding
 fs.readFile(filePath, 'utf-8', (err, data) => {
   if (err) {
     // Print the error object if an error occurs
-    console.error(err)
-    return
+    console.error(err);
+    return;
   }
 
   // Print the content of the file
-  console.log(data)
-})
+  console.log(data);
+});
 
