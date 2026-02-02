49d232cf3d2fb605005b1d8f893e28ea8b39982f
ehabimfura <e.habimfura@alustudent.com>
2026-02-02 07:19:16 -0800

    single quote

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const filePath = process.argv[2];
 
 if (!filePath) {
-    console.error("Please provide a file path as the first argument.");
+    console.error('Please provide a file path as the first argument.');
     process.exit(1);
 }
 
